a95fe377d219f8fb1f91213827be4a0002dfbb15
akushwah1987 <aakash.kushwah1987@outlook.com>
2022-11-12 17:42:24 +0530

    Initial commit

---
+++ src/main/java/com/sras/awas/AwasApplication.java
@@ -0,0 +1,13 @@
+package com.sras.awas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sras/awas/AwasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sras.awas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
